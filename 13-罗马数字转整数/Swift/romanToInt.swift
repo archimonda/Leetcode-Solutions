97f14f5f540b73376d5b18ec4893d03357034384
wang-yuchao <wangyuchaogeek@163.com>
2019-05-21 17:23:42 +0800

    romanToInt

--- 13-罗马数字转整数/Swift/romanToInt.swift
+++ 13-罗马数字转整数/Swift/romanToInt.swift
@@ -1,48 +1,39 @@
-//
 //  romanToInt.swift
-//  swiftTraining
-//
 //  Created by 王玉朝 on 2019/5/21.
-//  Copyright © 2019 鑫考教育. All rights reserved.
-//
+//  Copyright © 2019 王玉朝. All rights reserved.
 
-import UIKit
-
-class romanToInt: UIViewController {
-
-    func romanToInt(s: String) -> Int {
-        let dict = initDict()
-        let chars = [Character](s.reversed())
-        var res = 0
-        
-        for i in 0..<chars.count {
-            guard let current = dict[String(chars[i])] else {
-                return res
-            }
-            if i > 0 && current < dict[String(chars[i - 1])]!{
-                res -= current
-            } else {
-                res += current
-            }
+func romanToInt(s: String) -> Int {
+    let dict = initDict()
+    let chars = [Character](s.reversed())
+    var res = 0
+    
+    for i in 0..<chars.count {
+        guard let current = dict[String(chars[i])] else {
+            return res
         }
-        
-        return res
+        if i > 0 && current < dict[String(chars[i - 1])]!{
+            res -= current
+        } else {
+            res += current
+        }
     }
     
-    private func initDict() -> [String: Int] {
-        var dict = [String: Int]()
-        
-        dict["I"] = 1
-        dict["V"] = 5
-        dict["X"] = 10
-        dict["L"] = 50
-        dict["C"] = 100
-        dict["D"] = 500
-        dict["M"] = 1000
-        
-        return dict
-    }   
-    
-
-
+    return res
 }
+
+private func initDict() -> [String: Int] {
+    var dict = [String: Int]()
+    
+    dict["I"] = 1
+    dict["V"] = 5
+    dict["X"] = 10
+    dict["L"] = 50
+    dict["C"] = 100
+    dict["D"] = 500
+    dict["M"] = 1000
+    
+    return dict
+}
+
+
+
