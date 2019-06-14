d22c28d57d49c91e2288b529ffb17a1c0bed5edc
wang-yuchao <wangyuchaogeek@163.com>
2019-06-14 17:25:15 +0800

    ValidParentheses
    ValidParentheses

---
+++ 0020-有效的括号/Swift/ValidParentheses.swift
@@ -0,0 +1,29 @@
+//  LongestCommonPrefix.swift
+//  Created by 王玉朝 on 2019/6/14.
+//  Copyright © 2019 王玉朝. All rights reserved.
+
+class ValidParentheses {
+    func isValid(_ s: String) -> Bool {
+        var stack = [Character]()
+        
+        for char in s {
+            if char == "(" || char == "[" || char == "{" {
+                stack.append(char)
+            } else if char == ")" {
+                guard stack.count != 0 && stack.removeLast() == "(" else {
+                    return false
+                }
+            } else if char == "]" {
+                guard stack.count != 0 && stack.removeLast() == "[" else {
+                    return false
+                }
+            } else if char == "}" {
+                guard stack.count != 0 && stack.removeLast() == "{" else {
+                    return false
+                }
+            }
+        }
+        
+        return stack.isEmpty
+    }
+}
